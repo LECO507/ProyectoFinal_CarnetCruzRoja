cbac445e4d0a634cb7fc7f23ba43c03f0cd77c38
LECO ArTs <LECO ArTs>
2016-04-28 11:32:51 +0200

    Ejercicio 1.1 Completo

---
+++ src/com/ceste/CarnetCruzRoja.java
@@ -0,0 +1,72 @@
+package com.ceste;
+
+/**
+ * Created by leco2_000 on 27/4/2016.
+ */
+public class CarnetCruzRoja {
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private String provincia;
+    private String localidad;
+    private String servicio;
+    private String caducidad;
+
+    public CarnetCruzRoja(String dni){
+        this.dni = dni;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getDni() {
+        return caducidad;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public String getLocalidad() {
+        return localidad;
+    }
+
+    public String getServicio() {
+        return servicio;
+    }
+
+    public String getCaducidad() {
+        return caducidad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public void setLocalidad(String localidad) {
+        this.localidad = localidad;
+    }
+
+    public void setServicio(String servicio) {
+        this.servicio = servicio;
+    }
+
+    public void setCaducidad(String caducidad) {
+        this.caducidad = caducidad;
+    }
+    
+}
+
+
---
+++ src/com/ceste/Main.java
@@ -0,0 +1,12 @@
+package com.ceste;
+
+
+
+/**
+ * Created by leco2_000 on 28/4/2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
